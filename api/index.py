8b2ac9a1e3074f5e98e3121fdf726356f99ff89c
bala-ji1045 <balajisaisurya@gmail.com>
2025-11-30 22:20:58 +0530

    Configure project for Vercel deployment

--- api/index.py
+++ api/index.py
@@ -1,13 +1,11 @@
-from flask import Flask, request, jsonify, render_template
 import os
 import sys
 
 # Add the parent directory to sys.path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from app import app as flask_app
+from app import app
 
-# This is the Vercel serverless function handler
-def handler(request):
-    with flask_app.request_context(request):
-        return flask_app.full_dispatch_request()
+# Export the Flask app for Vercel
+# Vercel will use this as the WSGI application
+app = app
